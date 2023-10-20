d90badb21a3b8b5c47fd1aed3ae7516cb2718075
ameba4 <148567854+ameba4@users.noreply.github.com>
2023-10-20 18:35:01 +0300

    Add files via upload

---
+++ access/values.js
@@ -0,0 +1,11 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "17.04.2005"; // Дата рождения
+var fullname = "Хоміч Матвій Сергіович" // ФИО
+var name = "Матвій" // Имя
+
+// Паспорт
+var passport_id = "739265497" // Номер паспорта
